e1b8e5026bbf23bdb8d787ca973d6fffdad38cf1
saiteja604 <73585388+saiteja604@users.noreply.github.com>
2021-08-31 12:56:57 +0530

    Update App.java

--- src/main/java/exampleProj/App.java
+++ src/main/java/exampleProj/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Teja!" );
-        System.out.println( "How are you ?" );
+   String s1="Darling..! You are a nightmare dressed up like a daydream. ";  
+   String s2="  Some stories are meant to be incomplete...!";  
+   String s3=s1.concat(s2);  
+   System.out.println(s3); 
     }
 }

b49e88796cd0f05d08f143672ab14110f63f9425
saiteja604 <73585388+saiteja604@users.noreply.github.com>
2021-08-31 12:51:37 +0530

    Update App.java

--- src/main/java/exampleProj/App.java
+++ src/main/java/exampleProj/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Teja!" );
-        System.out.println( "How are you !" );
+        System.out.println( "How are you ?" );
     }
 }
